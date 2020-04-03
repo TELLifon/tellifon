cee2a4dae0c0a5eb4d749dfd7f7b0dfe032fd60b
Mathis Kretz <mathis@bespinian.io>
2020-04-04 00:06:28 +0200

    Add category detail view

--- src/App.js
+++ src/App.js
@@ -1,26 +1,183 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuIcon from "@material-ui/icons/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link as RouterLink
+} from "react-router-dom";
+import CategoryPage from "./CategoryPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const categories = [
+  {
+    id: "coffe-and-cake",
+    label: "Coffee and Cake",
+    icon: "local_cafe"
+  },
+  {
+    id: "meditation",
+    label: "Meditation",
+    icon: "local_cafe"
+  },
+  {
+    id: "sports",
+    label: "Sports",
+    icon: "local_cafe"
+  },
+  {
+    id: "music",
+    label: "Music",
+    icon: "local_cafe"
+  }
+];
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0
+    }
+  },
+  appBar: {
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth
+    }
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none"
+    }
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth
+  },
+  content: {
+    flexGrow: 1,
+    marginTop: theme.spacing(8),
+    padding: theme.spacing(3)
+  }
+}));
+
+function ResponsiveDrawer(props) {
+  const { container } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        {categories.map(c => (
+          <ListItem button key={c.id} component={RouterLink} to={`/${c.id}`}>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary={c.label} />
+          </ListItem>
+        ))}
+      </List>
     </div>
+  );
+
+  return (
+    <Router>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              className={classes.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap>
+              Responsive drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <nav className={classes.drawer} aria-label="mailbox folders">
+          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+          <Hidden smUp implementation="css">
+            <Drawer
+              container={container}
+              variant="temporary"
+              anchor={theme.direction === "rtl" ? "right" : "left"}
+              open={mobileOpen}
+              onClose={handleDrawerToggle}
+              classes={{
+                paper: classes.drawerPaper
+              }}
+              ModalProps={{
+                keepMounted: true // Better open performance on mobile.
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <Hidden xsDown implementation="css">
+            <Drawer
+              classes={{
+                paper: classes.drawerPaper
+              }}
+              variant="permanent"
+              open
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+        </nav>
+        <main className={classes.content}>
+          <Switch>
+            <Route path="/:categoryId">
+              <CategoryPage />
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+ResponsiveDrawer.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  container: PropTypes.any
+};
+
+export default ResponsiveDrawer;
---
+++ src/CategoryPage.js
@@ -0,0 +1,105 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+
+const categoryData = {
+  id: "",
+  icon: "cafe_local",
+  label: "Indoor Sports",
+  events: [
+    {
+      name: "Morning Yoga",
+      moderator: "Mathis Kretz",
+      startdate: "2020-04-04",
+      duration: "60",
+      description: "Llorem ipsum",
+      id: "123",
+      is_public: true,
+      image_src:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.kamalaya.com%2Ffileadmin%2Fuser_upload%2FWELLNESS-PROGRAMS-THAILAND%2Fyoga-thailand-2000x850%2Fyoga-retreats-thailand.jpg&f=1&nofb=1"
+    },
+    {
+      name: "Push-ups",
+      moderator: "Mathis Kretz",
+      startdate: "2020-04-05",
+      duration: "30",
+      description: "Llorem ipsum",
+      id: "234",
+      is_public: true,
+      image_src:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.kamalaya.com%2Ffileadmin%2Fuser_upload%2FWELLNESS-PROGRAMS-THAILAND%2Fyoga-thailand-2000x850%2Fyoga-retreats-thailand.jpg&f=1&nofb=1"
+    },
+    {
+      name: "Gymnastics",
+      moderator: "",
+      startdate: "2020-04-04",
+      duration: "45",
+      description: "Llorem ipsum",
+      id: "345",
+      is_public: true,
+      image_src:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.kamalaya.com%2Ffileadmin%2Fuser_upload%2FWELLNESS-PROGRAMS-THAILAND%2Fyoga-thailand-2000x850%2Fyoga-retreats-thailand.jpg&f=1&nofb=1"
+    },
+    {
+      name: "Dance",
+      moderator: "Mathis Kretz",
+      startdate: "2020-04-06",
+      duration: "15",
+      description: "Llorem ipsum",
+      id: "456",
+      is_public: true,
+      image_src:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.kamalaya.com%2Ffileadmin%2Fuser_upload%2FWELLNESS-PROGRAMS-THAILAND%2Fyoga-thailand-2000x850%2Fyoga-retreats-thailand.jpg&f=1&nofb=1"
+    }
+  ]
+};
+
+const useStyles = makeStyles({
+  media: {
+    height: 140
+  }
+});
+
+const CategoryPage = () => {
+  // const { categoryId } = useParams();
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Grid container spacing={3}>
+        {categoryData.events.map(e => (
+          <Grid item xs={12} sm={6} lg={4} xl={3}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={e.image_src}
+                  title={e.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {e.name}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {e.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+export default CategoryPage;
