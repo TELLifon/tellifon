73cc708720093829c98504a0ea5e0b95a934fced
Tobi Fuhrimann <tobias.fuhrimann@gmail.com>
2020-04-05 00:19:59 +0200

    Clean up creation form

--- src/CreateEventDialog.js
+++ src/CreateEventDialog.js
@@ -18,23 +18,39 @@ import {
 import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
-  imagePreview: { maxWidth: theme.spacing(32) },
+  imagePreview: {
+    marginTop: theme.spacing(2),
+    maxHeight: theme.spacing(24),
+    objectFit: "cover",
+    width: "100%",
+  },
+  dropzone: {
+    border: "1px dashed gray",
+    padding: theme.spacing(5),
+    textAlign: "center",
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: "#f1f1f1",
+    },
+  },
 }));
 
 const CreateEventModal = ({ isOpen, handleClose, categoryId }) => {
-  const now = new Date();
   const classes = useStyles();
+
+  const now = new Date();
   const inOneHour = new Date();
   inOneHour.setHours(inOneHour.getHours() + 2);
-  const [name, setName] = useState("");
+  const [title, setTitle] = useState("");
   const [startTime, setStartTime] = useState(now);
-  const [startDatePickerOpen, setStartDatePickerOpen] = useState(false);
+  const [isStartDatePickerOpen, setIsStartDatePickerOpen] = useState(false);
   const [endTime, setEndTime] = useState(inOneHour);
-  const [endDatePickerOpen, setEndDatePickerOpen] = useState(false);
+  const [isEndDatePickerOpen, setIsEndDatePickerOpen] = useState(false);
   const [description, setDescription] = useState("");
   const [moderatorName, setModeratorName] = useState("");
   const [isPublic, setIsPublic] = useState(true);
   const [image, setImage] = useState(null);
+
   const onDrop = useCallback((acceptedFiles) => {
     setImage(
       Object.assign(acceptedFiles[0], {
@@ -49,7 +65,7 @@ const CreateEventModal = ({ isOpen, handleClose, categoryId }) => {
 
   const handleCreate = async () => {
     const formData = new FormData();
-    formData.append("name", name);
+    formData.append("name", title);
     formData.append("starttime", startTime);
     formData.append("endtime", endTime);
     formData.append("description", description);
@@ -66,10 +82,6 @@ const CreateEventModal = ({ isOpen, handleClose, categoryId }) => {
     handleClose();
   };
 
-  const preview = image ? (
-    <img src={image.preview} alt="preview" className={classes.imagePreview} />
-  ) : null;
-
   return (
     <Dialog
       open={isOpen}
@@ -82,35 +94,78 @@ const CreateEventModal = ({ isOpen, handleClose, categoryId }) => {
         <TextField
           autoFocus
           margin="normal"
-          id="name"
-          label="Name"
+          id="title"
+          label="Title"
           fullWidth
-          value={name}
+          value={title}
           onChange={(e) => {
-            setName(e.target.value);
+            setTitle(e.target.value);
+          }}
+        />
+        <TextField
+          margin="normal"
+          id="description"
+          label="Description"
+          multiline
+          fullWidth
+          value={description}
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+        />
+        <TextField
+          margin="normal"
+          id="moderator-name"
+          label="Moderator Name (Optional)"
+          fullWidth
+          value={moderatorName}
+          onChange={(e) => {
+            setModeratorName(e.target.value);
           }}
         />
         <br />
-        <div {...getRootProps()}>
-          <input {...getInputProps()} />
-          {isDragActive ? (
-            <p>Drop the image here</p>
-          ) : (
-            <p>Drag and drop or click to select an image</p>
-          )}
-        </div>
-        {preview}
+        <br />
+        {image ? (
+          <>
+            <img
+              src={image.preview}
+              alt="preview"
+              className={classes.imagePreview}
+            />
+            <Button
+              color="secondary"
+              onClick={() => {
+                setImage(null);
+              }}
+            >
+              Clear Image
+            </Button>
+          </>
+        ) : (
+          <div className={classes.dropzone} {...getRootProps()}>
+            <input {...getInputProps()} />
+            {isDragActive ? (
+              <p>Drop the image here</p>
+            ) : (
+              <p>Drag and drop or click to select an image</p>
+            )}
+          </div>
+        )}
         <br />
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
           <Grid container justify="space-around">
             <KeyboardDatePicker
               disableToolbar
-              open={startDatePickerOpen}
-              onOpen={() => setStartDatePickerOpen(true)}
-              onClose={() => setStartDatePickerOpen(false)}
+              open={isStartDatePickerOpen}
+              onOpen={() => {
+                setIsStartDatePickerOpen(true);
+              }}
+              onClose={() => {
+                setIsStartDatePickerOpen(false);
+              }}
               onChange={(d) => {
                 setStartTime(d.toISOString());
-                setStartDatePickerOpen(false);
+                setIsStartDatePickerOpen(false);
               }}
               label="Start date"
               variant="inline"
@@ -147,12 +202,16 @@ const CreateEventModal = ({ isOpen, handleClose, categoryId }) => {
               margin="normal"
               id="date-picker-inline"
               value={endTime}
-              open={endDatePickerOpen}
-              onOpen={() => setEndDatePickerOpen(true)}
-              onClose={() => setEndDatePickerOpen(false)}
+              open={isEndDatePickerOpen}
+              onOpen={() => {
+                setIsEndDatePickerOpen(true);
+              }}
+              onClose={() => {
+                setIsEndDatePickerOpen(false);
+              }}
               onChange={(d) => {
                 setEndTime(d.toISOString());
-                setEndDatePickerOpen(false);
+                setIsEndDatePickerOpen(false);
               }}
               KeyboardButtonProps={{
                 "aria-label": "change date",
@@ -172,28 +231,7 @@ const CreateEventModal = ({ isOpen, handleClose, categoryId }) => {
             />
           </Grid>
         </MuiPickersUtilsProvider>
-        <TextField
-          margin="normal"
-          id="description"
-          label="Description"
-          multiline
-          fullWidth
-          value={description}
-          onChange={(e) => {
-            setDescription(e.target.value);
-          }}
-        />
         <br />
-        <TextField
-          margin="normal"
-          id="moderator-name"
-          label="Moderator Name (Optional)"
-          fullWidth
-          value={moderatorName}
-          onChange={(e) => {
-            setModeratorName(e.target.value);
-          }}
-        />
         <br />
         <FormControlLabel
           control={
@@ -209,10 +247,10 @@ const CreateEventModal = ({ isOpen, handleClose, categoryId }) => {
         />
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose} color="primary">
+        <Button color="primary" onClick={handleClose}>
           Cancel
         </Button>
-        <Button onClick={handleCreate} color="primary">
+        <Button color="primary" variant="contained" onClick={handleCreate}>
           Create
         </Button>
       </DialogActions>
