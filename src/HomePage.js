1a5ae6ebbb28c6d762085eaa1b508bf3356a1311
Mathis Kretz <mathis@bespinian.io>
2020-04-05 18:45:41 +0200

    Add Tellifon logo to home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,10 +2,14 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import { useTranslation } from "react-i18next";
+import tellifonLogo from "./tellifon.png";
 
 const useStyles = makeStyles((theme) => ({
   container: {
     maxWidth: theme.spacing(70),
+  },
+  logo: {
+    maxHeight: theme.spacing(20),
   },
 }));
 
@@ -18,6 +22,9 @@ const HomePage = () => {
       <Typography variant="h4" gutterBottom>
         {t("home.title")}
       </Typography>
+      <br />
+      <img src={tellifonLogo} className={classes.logo} alt="Tellifon logo" />
+      <br />
       <br />
       <Typography>{t("home.text")}</Typography>
       <br />
