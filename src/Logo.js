8648fb9272fe961a8e6f52e9a73c3a07f7e7c4d0
Tobi Fuhrimann <tobias.fuhrimann@gmail.com>
2020-04-05 20:29:02 +0200

    Link logo to home page

--- src/Logo.js
+++ src/Logo.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import logo from "./tellifon-white.png";
+import { Link as RouterLink } from "react-router-dom";
 
 const useStyles = makeStyles({
   container: {
@@ -19,7 +20,7 @@ const Logo = () => {
 
   return (
     <div className={classes.container}>
-      <Button>
+      <Button component={RouterLink} to="/">
         <img className={classes.img} src={logo} alt="Tellifon" />
       </Button>
     </div>
